8590ce8aebac486aa47e1175fc04c3de5c0f506e
neranjaka <neranjaka@yahoo.com>
2015-03-23 00:36:15 +0530

    third commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL           #inverse is assigned null
+  inv <<- NULL           #inverse is assigned null
   set <- function(y) {   #the function to set the matrix
     x <<- y              # the matrix y is chached in variable x
     inv <<- NULL
